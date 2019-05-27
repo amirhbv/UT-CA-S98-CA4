423a3f00d3ddbcb2a470ac56d6eb2599d4e32373
AmirHossein Habibvand <ahhabibvand@gmail.com>
2019-05-27 15:01:52 +0430

    Change instruction memory size

--- instruction_memory.sv
+++ instruction_memory.sv
@@ -1,9 +1,13 @@
 `timescale 1ns/1ns
 
 module InstructionMemory(
-    input[9:0] address,
+    input[31:0] addr,
     output[31:0] inst_out
 );
-    reg[31:0] mem[0:1023] ;
-    assign inst_out = mem[address] ;
+    reg[7:0] mem[0:1023] ;
+
+	wire address;
+	assign address = addr[9:0];
+
+	assign inst_out = {instMem[address], instMem[address + 1], instMem[address + 2], instMem[address + 3]};
 endmodule
