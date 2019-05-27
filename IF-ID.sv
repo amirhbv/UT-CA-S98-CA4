c6283f0b7c0fd73ad8431486117734523cc69509
AmirHossein Habibvand <ahhabibvand@gmail.com>
2019-05-27 15:17:09 +0430

    Change pc to pc_in , Fix sign extend bug

--- IF-ID.sv
+++ IF-ID.sv
@@ -3,26 +3,27 @@
 module IF_ID(
     input clk, rst,
     input[31:0] instruction,
-    input[9:0]PC,
+    input[31:0]PC_in,
     input flush,
-    output reg[9:0] PC_out,
+    output reg[31:0] PC_out,
     output reg [31:0] inst_out,
     output[31:0] instr15_0
 );
     reg[31:0] reg_inst_out ;
-    reg[ 9:0] reg_PC_out ;
+    reg[ 31:0] reg_PC_out ;
 
-    assign instr15_0 = {16'b0 , instruction[15:0]} ;
+	assign instr15_0 = (instruction[15] == 1) ? {16'b1111111111111111, instruction} : {16'b0000000000000000, instruction};
+
     always @(posedge clk , posedge rst) begin
         if ( rst ) begin
             reg_PC_out <= 10'b0 ;
             reg_inst_out <= 32'b0 ;
         end else begin
             reg_inst_out <= instruction ;
-            reg_PC_out <= PC ;
+            reg_PC_out <= PC_in ;
             if ( flush == 0 ) begin
                 inst_out <= instruction ;
-                PC_out <= PC ;
+                PC_out <= PC_in ;
             end else begin
                 inst_out <= 32'b0 ;
                 // PC_out   <= 10'b0 ; todo: check if PC is beeded here
