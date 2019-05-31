ce1d53aa619db7412096f49cfee845ab01d45fff
AmirHossein Habibvand <ahhabibvand@gmail.com>
2019-05-31 17:30:47 +0430

    Make hazzard detection clocked

--- HazardDetection.sv
+++ HazardDetection.sv
@@ -1,6 +1,7 @@
 `timescale 1ns/1ns
 
 module HazardDetection(
+	input clk,
     input[4:0]  Rt_IF_ID,
                 Rs_IF_ID,
                 Rt_ID_EX,
@@ -9,25 +10,25 @@ module HazardDetection(
     input[31:0] offset_ID_EX,
     input       comparatorForBranch,
     input       isJump,
-    
+
     output reg  holdPC,
                 IF_ID_Flush,
                 isBranch,
     output reg[31:0] PC_offset
 );
-    reg[1:0] cnt;
-    always @(*) begin
-        { holdPC, IF_ID_Flush } = 0;
+    always @(posedge clk) begin
+        { holdPC, IF_ID_Flush, isBranch } = 0;
         if ( signals_M_ID_EX[1] == 1) begin
-            if (Rt_ID_EX == Rs_IF_ID || Rt_ID_EX == Rt_IF_ID)
+            if (Rt_ID_EX == Rs_IF_ID || Rt_ID_EX == Rt_IF_ID) begin
                 holdPC = 1;
                 IF_ID_Flush = 1;
+            end
         end
-        
+
         if ( isJump ) begin
           IF_ID_Flush = 1 ;
         end
-        
+
         /*
         // beq, bne case
         if ( signals_M_Controller[2] == 1 || signals_M_Controller[3] == 1) begin
@@ -35,9 +36,9 @@ module HazardDetection(
             IF_ID_Flush = 1;
         end
         */
-        
+
         if (
-            (signals_M_ID_EX[2] == 1 && comparatorForBranch) ||
+            (signals_M_ID_EX[2] == 1 &&  comparatorForBranch) ||
             (signals_M_ID_EX[3] == 1 && ~comparatorForBranch)
         ) begin
             isBranch = 1;
